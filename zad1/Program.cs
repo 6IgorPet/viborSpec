007adcc3639eb0910a8397bac4bcfc9eae4c7724
igor <desant350@gmail.com>
2022-08-05 19:49:36 +0200

    новый масив

---
+++ zad1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* **Задача**: Написать программу, которая из имеющегося массива
+строк формирует массив из строк, длина которых меньше либо 
+равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении 
+не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+**Примеры**:
+[hello,2,world,:-)] -> ["2", ":-)"]
+[1234,1567,-2,computer science] -> ["-2"]
+[Russia,Denmark,Kazan] -> [] */
+
+string[] result = CreateArray();
+ArraySort(result);
+
+void ArraySort(string[] arr)
+{
+    int size = 3; // условия для отбора элементов из маассива
+    var value = string.Empty;
+    string[] newarr = new string[arr.Length]; // создать новый массив для отобранных элементов
+    for (int i = 0; i < arr.Length; i++)
+    {
+        value = arr[i]; 
+        if (value.Length <= size) // если элемент на позиции меньше условия для отбора
+            newarr[i] = arr[i];
+    }
+    newarr = newarr.Where(x => x != null).ToArray(); // чтобы убрать пустые позиции массива 
+    Console.WriteLine($"Отобранный массив: [{string.Join(", ", newarr)}]");
+}
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите элементы массива через запятую: ");
+    string[] array = Console.ReadLine()!.Split(',');
+    Console.WriteLine($"Изначальный массив: [{string.Join(", ", array)}]");
+    return array;
+}
